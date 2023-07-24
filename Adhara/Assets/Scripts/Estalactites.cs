3f376690fd41a5969c2a0165ac9ec366f2bff839
JOÃO PAULO VICENTE LEÃO <j.leao@academico.ifrn.edu.br>
2023-07-24 13:03:40 -0300

    tenho q arrumar o andar do personagem

---
+++ Adhara/Assets/Scripts/Estalactites.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Estalactites : MonoBehaviour
+{
+    private Rigidbody2D rig;
+    public Collider2D collisor;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            rig.bodyType = RigidbodyType2D.Dynamic;
+            rig.gravityScale = Random.Range(2f, 8f);
+            Destroy(this.gameObject, 1.5f);
+        }
+    }
+}
---
+++ Adhara/Assets/Scripts/PlataformasQueCaem.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformasQueCaem : MonoBehaviour
+{
+
+    private Rigidbody2D rig;
+
+    private float fallDelay;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(0.2f);
+        rig.bodyType = RigidbodyType2D.Dynamic;
+        Destroy(gameObject, fallDelay);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine("Fall");
+        }
+    }
+}
